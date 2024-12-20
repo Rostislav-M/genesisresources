4c9adcc7b2d8372966b4c2d7e26b0862b3a8690f
Rostislav-M <rostislav.malatak@gmail.com>
2024-12-20 04:27:42 +0100

    -

---
+++ src/main/resources/db/create-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+  	id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NULL,
+    person_id VARCHAR(12) NOT NULL,
+    uuid VARCHAR(36) NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id),
+    CONSTRAINT users_unique_person_id UNIQUE (person_id),
+    CONSTRAINT users_unique_uuid UNIQUE (uuid)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
